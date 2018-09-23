5dafdbcd7a9cce7dc744840e8a2462de3c048539
RyansHDs#4461 <39214741+ryanshdsdev@users.noreply.github.com>
2018-09-23 09:37:30 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,25 +3,14 @@ const send = require('quick.hook')
 
 module.exports = async (client, message) => {
   // Log that the bot is online.
-  const chnl = client.channels.get('462800219998912522')
   client.logger.log(`[READY] ${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
-  let onlinestats = new Discord.RichEmbed()
-  .setAuthor(`${client.user.username}`, client.user.displayAvatarURL)
-  .setTitle(`Bot has been restarted!`)
-  .setThumbnail(client.user.displayAvatarURL)
-  .setDescription(`${client.user.username} Has been restarted!\nCaused by: Hosting / Updated.`)
-  .setFooter(`Time before restart ->`)
-  .setTimestamp()
-  .setColor(`#d10a0c`)
-  chnl.send(onlinestats)
-  
     function status(){
     let status = [
         `.help | ${client.guilds.size} Guilds`,
       `.help | ${client.users.size} Users`,
     ]
     let rstatus = Math.floor(Math.random() * status.length);
-    client.user.setActivity(status[rstatus], {type:"PLAYING", url:"https://twitch.tv/ryanshds"})
+    client.user.setActivity(status[rstatus], {type:"PLAYING", url:"https://twitch.tv/twitch"})
 }; setInterval(status, 60000)
 
 };
