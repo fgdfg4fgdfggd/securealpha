47c855635fa8d660d1e96bc761421b27ea6d4491
fgdfg4fgdfggd <50755286+fgdfg4fgdfggd@users.noreply.github.com>
2020-01-14 06:10:32 -0600

    Update reboot.js

--- commands/reboot.js
+++ commands/reboot.js
@@ -12,7 +12,7 @@ exports.run = async (client, message, args, level) => {// eslint-disable-line no
   });
   process.exit(1);
     } catch(err) {
-      const errorlogs = client.channels.get('464424869497536512')
+      const errorlogs = client.channels.get('666545299778568192')
       message.channel.send(`Whoops, We got a error right now! This error has been reported to Support center!`)
       errorlogs.send(`Error on reboot commands!\n\nError:\n\n ${err}`)
     }
