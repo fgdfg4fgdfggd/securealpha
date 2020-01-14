debe15b1702fc2cafd380d5892de2019aea9513b
fgdfg4fgdfggd <50755286+fgdfg4fgdfggd@users.noreply.github.com>
2020-01-14 02:33:17 -0600

    Update bugreport.js

--- commands/bugreport.js
+++ commands/bugreport.js
@@ -25,7 +25,7 @@ exports.run = (client, message) => {
         cooldown.delete(message.author.id);
     }, 300000);
     let guild = message.guild;
-    const cnl = client.channels.get('449130025627680768');
+    const cnl = client.channels.get('666559923399688204');
     message.reply('We got your report! Full report:');
     const embed2 = new Discord.RichEmbed()
   .setAuthor(`Report info ${message.author.tag}`, message.author.displayAvatarURL)
@@ -41,12 +41,12 @@ exports.run = (client, message) => {
   .setColor(16711728)
   .setFooter(`${moment().format('MMMM Do YYYY, h:mm:ss a')}`)
     send(cnl, embed, {
-        name: `RyanBot Bugreport`,
-        icon: `https://cdn.discordapp.com/attachments/421620705570979843/462632872616919053/R-logo512.png`
+        name: `Phantom Bugreport`,
+        icon: `https://cdn.discordapp.com/attachments/666560077226049546/666560191130501150/phantomlogo.png`
     })
   .catch(e => client.logger.error(e))
     } catch(err) {
-      const errorlogs = client.channels.get('464424869497536512')
+      const errorlogs = client.channels.get('666545299778568192')
       message.channel.send(`Whoops, We got a error right now! This error has been reported to Support center!`)
       errorlogs.send(`Error on bugreport commands!\n\nError:\n\n ${err}`)
     }
