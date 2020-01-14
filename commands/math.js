b453f66a7388f38d7e3970de1d949273715732cd
fgdfg4fgdfggd <50755286+fgdfg4fgdfggd@users.noreply.github.com>
2020-01-14 07:06:14 -0600

    Update math.js

--- commands/math.js
+++ commands/math.js
@@ -36,7 +36,7 @@ exports.run = (client, message, args, tools) => {
     message.channel.send(embed);
     
       } catch(err) {
-      const errorlogs = client.channels.get('464424869497536512')
+      const errorlogs = client.channels.get('666545299778568192')
       message.channel.send(`Whoops, We got a error right now! This error has been reported to Support center!`)
       errorlogs.send(`Error on math commands!\n\nError:\n\n ${err}`)
     }
