43ed91901e05070fbbc4b4351612dbb869a8c833
fgdfg4fgdfggd <50755286+fgdfg4fgdfggd@users.noreply.github.com>
2020-01-14 06:32:47 -0600

    Update cowsay.js

--- commands/cowsay.js
+++ commands/cowsay.js
@@ -34,7 +34,7 @@ exports.run = (client, message, args) => {
         e: 'oO'
     }), {code: 'css'});
     } catch(err) {
-      const errorlogs = client.channels.get('464424869497536512')
+      const errorlogs = client.channels.get('666545299778568192')
       message.channel.send(`Whoops, We got a error right now! This error has been reported to Support center!`)
       errorlogs.send(`Error on cowsay commands!\n\nError:\n\n ${err}`)
     }
