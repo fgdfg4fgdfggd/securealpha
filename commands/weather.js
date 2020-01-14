cd27c1958abd38a72a3f0e3f25b1cfc472cd55a3
fgdfg4fgdfggd <50755286+fgdfg4fgdfggd@users.noreply.github.com>
2020-01-14 07:22:22 -0600

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -25,7 +25,7 @@ module.exports.run = (client, message, args) => {
           message.channel.send({embed});
   })
     } catch(err) {
-      const errorlogs = client.channels.get('464424869497536512')
+      const errorlogs = client.channels.get('666545299778568192')
       message.channel.send(`Whoops, We got a error right now! This error has been reported to Support center!`)
       errorlogs.send(`Error on weather commands!\n\nError:\n\n ${err}`)
     }
