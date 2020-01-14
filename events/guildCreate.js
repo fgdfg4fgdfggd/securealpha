e877c2c3ad6e51eccc570c0404c201990a8dc02c
fgdfg4fgdfggd <50755286+fgdfg4fgdfggd@users.noreply.github.com>
2020-01-14 07:24:50 -0600

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,10 +2,10 @@ const discord = require("discord.js")
 const send = require('quick.hook')
 
 module.exports = (client, guild) => {
-    const rbnjoin = client.channels.get("449130097136369664"); //CHANGE TO YOUR CHANNEL-ID TO GET NOTIFICATIONS
+    const rbnjoin = client.channels.get("666633703409057793"); //CHANGE TO YOUR CHANNEL-ID TO GET NOTIFICATIONS
     let rbnEmbed = new discord.RichEmbed()
     .setAuthor(client.user.username, client.user.avatarURL)
-    .setTitle(`RyanBot started serving **${guild.name}**`)
+    .setTitle(`Phantom started serving **${guild.name}**`)
     .setThumbnail(guild.iconURL)
     .setDescription(`**Guild Owner**: ${guild.owner}\n**Guild Name**: ${guild.name}\n**Guild ID**: ${guild.id}\n**Guild Channels Count**: ${guild.channels.size} \n**Members Gained**: ${guild.memberCount}`)
     rbnjoin.send(rbnEmbed)
