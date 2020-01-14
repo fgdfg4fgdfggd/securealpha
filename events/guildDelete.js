414dc1e5b3bf200eb5ac8c4a0b918591dfd71319
fgdfg4fgdfggd <50755286+fgdfg4fgdfggd@users.noreply.github.com>
2020-01-14 07:25:15 -0600

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -2,11 +2,11 @@ const discord = require("discord.js")
 const send = require('quick.hook')
 
 module.exports = async (client, guild) => {
-    const rbnleave = client.channels.get("449130097136369664"); //CHANGE TO YOUR CHANNEL-ID TO GET NOTIFICATIONS
+    const rbnleave = client.channels.get("666633703409057793"); //CHANGE TO YOUR CHANNEL-ID TO GET NOTIFICATIONS
     let rbnembed = new discord.RichEmbed()
     .setAuthor(client.user.username, client.user.avatarURL)
     .setThumbnail(guild.iconURL)
-    .setTitle(`RyanBot Has stopped serving **${guild.name}**`)
+    .setTitle(`Phantom Has stopped serving **${guild.name}**`)
     .setDescription(`**Guild Owner**: ${guild.owner}\n**Guild Name**: ${guild.name}\n**Guild ID**: ${guild.id}\n**Guild Channels Count**: ${guild.channels.size} \n**Members Lost**: ${guild.memberCount}`)
     rbnleave.send(rbnembed)
 }
